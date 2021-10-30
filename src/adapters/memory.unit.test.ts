dff5c3c6edbf7a8aabb3f151df9669fc8b51aa93
waddah-jd <waddah.ja@gmail.com>
2021-10-30 13:06:10 +0300

    add Memory Adapter unit test

---
+++ src/adapters/memory.unit.test.ts
@@ -0,0 +1,16 @@
+import MemoryAdapter from "./memory";
+
+describe("Memory Adapter", () => {
+  it("should be created with required 'public' methods", () => {
+    const adapter = new MemoryAdapter();
+
+    expect(adapter).toBeDefined();
+    expect(adapter).toBeInstanceOf(MemoryAdapter);
+    expect(adapter.init).toBeInstanceOf(Function);
+    expect(adapter.connect).toBeInstanceOf(Function);
+    expect(adapter.disconnect).toBeInstanceOf(Function);
+    expect(adapter.exists).toBeInstanceOf(Function);
+    expect(adapter.get).toBeInstanceOf(Function);
+    expect(adapter.set).toBeInstanceOf(Function);
+  });
+});
