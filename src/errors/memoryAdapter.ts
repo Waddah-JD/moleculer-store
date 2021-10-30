1078f4c498eec70df375fd9bec58e3f10b5c13e3
waddah-jd <waddah.ja@gmail.com>
2021-10-30 16:55:57 +0300

    extend Moleculer error for NonExistingKeyError

--- src/errors/memoryAdapter.ts
+++ src/errors/memoryAdapter.ts
@@ -1,6 +1,11 @@
-export class NonExistingKeyError extends Error {
+import { Errors } from "moleculer";
+
+const { MoleculerError } = Errors;
+
+export class NonExistingKeyError extends MoleculerError {
   constructor(key: Key) {
     super(`key '${key}' doesn't exist`);
     this.name = "NonExistingKeyError";
+    this.code = 204;
   }
 }
